0acab81fac1b9ea753db40eefb78c3f70166baa7
Arman4102 <arjunamuhammad011@gmail.com>
2025-06-29 21:14:08 +0700

    first commit

---
+++ anything.py
@@ -0,0 +1,7 @@
+# Array di python disebut list
+buahs = ['Semangka', 'Ikan', 'Pepaya', "Mangga"]
+counter = 0
+print("List Buah Buahan: ")
+for buah in buahs:
+    counter += 1
+    print(f"No {counter}. {buah}")
