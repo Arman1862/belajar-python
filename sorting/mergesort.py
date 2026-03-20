e74ebfe999bae67df98909416472f3b2e629ae7d
Arman4102 <pinil1862@gmail.com>
2026-03-20 23:56:28 +0700

    complete the mergesort

---
+++ sorting/mergesort.py
@@ -0,0 +1,30 @@
+def mergesort(S):
+    n = len(S)
+    if n > 1:
+        print(S)
+        mid = n // 2
+        L, R = S[:mid], S[mid:]
+        mergesort(L)
+        mergesort(R)
+        merge1(S, L, R)
+
+def merge1(S, L, R):
+    k = 0
+    while len(L) > 0 and len(R) > 0:
+        if L[0] <= R[0]:
+            S[k] = L.pop(0)
+        else:
+            S[k] = R.pop(0)
+        k += 1
+
+    while len(L) != 0:
+        S[k] = L.pop(0)
+        k += 1
+    while len(R) != 0:
+        S[k] = R.pop(0)
+        k += 1
+    
+S = [27,10,12,20,25,13,15,22]
+print(f"list sebelum di sortir:", S)
+mergesort(S)
+print(f"list sesudah di sortir:", S)
